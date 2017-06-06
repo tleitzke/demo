216fa0a914d808f52bac57f01f3f4a3100ac2826
AMS\daniel.stashek <daniel.stashek@cgi.com>
2017-06-06 13:51:32 -0600

    fixed enable config server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableConfigServer
 @SpringBootApplication
 public class DemoApplication {
 	
-	@EnableConfigServer
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
